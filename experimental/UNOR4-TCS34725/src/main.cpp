1f5e552e98e2aabfb26c72502f1ab4d126ef6719
shirokuma89dev <code@shirokuma89.dev>
2023-12-22 10:41:50 +0900

    💩 コンパイル通らない。おそらくUNO R4固有の問題

--- experimental/UNOR4-TCS34725/src/main.cpp
+++ experimental/UNOR4-TCS34725/src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+class TCS34725 {
+   private:
+    const char Address = 0x29;
+    TwoWire *i2cPtr;
+
+   public:
+    TCS34725(TwoWire *wire) {
+        this->i2cPtr = wire;
+    }
+
+    void init() {
+        i2cPtr->begin();
+        i2cPtr->beginTransmission(Address);
+
+        delay(100);
+        i2cPtr->write(0b10000000 | 0x12);
+        i2cPtr->endTransmission(false);
+        i2cPtr->requestFrom(Address, 1);
+
+        delay(100);
+
+        if (i2cPtr->available() >= 0) {
+            id = i2cPtr->read();
+        }
+    }
+
+    char id = NULL;
+};
+
+TCS34725 colorSensor(&Wire);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    colorSensor.init();
+    Serial.print("TCS34725固有ID:");
+    Serial.println(colorSensor.id, HEX);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Serial.println("loop");
 }
