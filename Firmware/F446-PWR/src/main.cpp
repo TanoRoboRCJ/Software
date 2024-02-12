9e2c1f766e7b042271786e54a99bd4d8ffb57b10
shirokuma89dev <code@shirokuma89.dev>
2024-02-12 22:04:42 +0900

    ✨ TCS34725の読み取りに成功

--- Firmware/F446-PWR/src/main.cpp
+++ Firmware/F446-PWR/src/main.cpp
@@ -1,67 +1,85 @@
 #include <Arduino.h>
+#include <VL53L0X.h>
 
-// #include <IO-Kit.h>
-// Output ledBuiltin = Output(PB12);
-// Output ledL = Output(PB14);
-// // Output ledBuiltin = Output(PA0);
+#include <IO-Kit.h>
 
-// Output encLED[3] = {Output(PB4), Output(PB5), Output(PB6)};
-// Input enc[2] = {Input(PA14), Input(PA15)};
-
-// HardwareSerial uart1(PA10, PA9);
-
-// bool chickPeep(void) {
-//     return (millis() / 20) % 2 == 0 && (millis() / 300) % 5 == 0;
-// }
-
-// void setup() {
-//     uart1.begin(115200);
-//     for (int i = 0; i < 3; i++) {
-//         encLED[i] = 1;
-//     }
-// }
-
-// void loop() {
-//     ledBuiltin = chickPeep();
-//     ledL = (millis() % 1000 < 500) * 1;
-// }
+VL53L0X distanceSensor[2];
+Output xshutPin[2] = {Output(PA5), Output(PA6)};
+const int addr[2] = {0x30, 0x31};
 
 #include <Wire.h>
-#include <Adafruit_TCS34725.h>
+#include <SPI.h>
+#include "./lib/Adafruit_TCS34725.h"
 
-/* Initialise with specific int time and gain values */
 Adafruit_TCS34725 tcs =
-    Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+    Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_60MS, TCS34725_GAIN_1X);
+
+HardwareSerial uartForDebug(PA10, PA9);
+
+void invalidA(void) {
+    while (1) {
+        uartForDebug.println("invalid A: VL53L0X ERR");
+        delay(100);
+    }
+}
+
+void invalidB(void) {
+    while (1) {
+        uartForDebug.println("invalid B: TCS34725 ERR");
+        delay(100);
+    }
+}
+
+void* invalid[2];
 
 void setup(void) {
-    Serial.begin(115200);
+    uartForDebug.begin(115200);
 
-    // I2Cピン設定 I2C0を使用する
-    Wire.setSDA(0);
-    Wire.setSCL(1);
+    Wire.setSDA(PB9);
+    Wire.setSCL(PB8);
+    Wire.begin();
 
-    if (tcs.begin()) {
-        Serial.println("Found sensor");
-    } else {
-        Serial.println("No TCS34725 found ... check your connections");
-        while (1)
-            ;
+    for (int i = 0; i < 2; i++) {
+        xshutPin[i] = LOW;
+    }
+
+    delay(100);
+
+    for (int i = 0; i < 2; i++) {
+        xshutPin[i] = HIGH;
+        delay(10);
+        distanceSensor[i].setTimeout(100);
+        distanceSensor[i].setAddress(addr[i]);
+        if (!distanceSensor[i].init()) {
+            invalidA();
+        }
+        distanceSensor[i].setSignalRateLimit(0.1);
+        distanceSensor[i].setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+        distanceSensor[i].setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange,
+                                              14);
+        distanceSensor[i].startContinuous();
+    }
+
+    if (!tcs.begin()) {
+        invalidB();
     }
 }
 
 void loop(void) {
-    uint16_t r, g, b, c;
+    uint16_t color[3] = {0};
+    uint16_t brightness = 0;
 
-    tcs.getRawData(&r, &g, &b, &c);
+    tcs.getRawData(&color[0], &color[1], &color[2], &brightness);
 
-    Serial.print("R: ");
-    Serial.print(r, DEC);
-    Serial.print(" ");
-    Serial.print("G: ");
-    Serial.print(g, DEC);
-    Serial.print(" ");
-    Serial.print("B: ");
-    Serial.print(b, DEC);
-    Serial.print(" ");
-    Serial.println(" ");
+    // Print
+    uartForDebug.printf("TCS34725:");
+    for (int i = 0; i < 3; i++) {
+        uartForDebug.printf("%6d", color[i]);
+    }
+    uartForDebug.printf("%6d", brightness);
+
+    int rawData = distanceSensor[0].readRangeContinuousMillimeters();
+    uartForDebug.printf(" | VL53L0X 1:%4d [mm]", rawData);
+    rawData = distanceSensor[1].readRangeContinuousMillimeters();
+    uartForDebug.printf(" | VL53L0X 2:%4d [mm]\n", rawData);
 }
