5cec1ff9a72b1374108434fe4c414087645be806
onagi37s <suminagiotibi@icloud.com>
2024-07-10 21:26:49 +0900

    ✅ 立体対応(暫定)

--- main/F446-RAICHO/src/RTOS/daemon.cpp
+++ main/F446-RAICHO/src/RTOS/daemon.cpp
@@ -112,8 +112,8 @@ void monitorApp(App) {
         // uart1.print("gyro: ");
         // uart1.print(gyro.deg);
         // uart1.print("\t");
-        uart1.print(gyro.slope);
-        uart1.print("\t");
+        // uart1.print(gyro.slope);
+        // uart1.print("\t");
         // uart1.print(tof.lidarRightWallExists);
 
         // uart1.print("\t");
